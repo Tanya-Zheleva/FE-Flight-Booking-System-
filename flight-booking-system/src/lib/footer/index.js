676f2114f02dd3f0736d6306afc65b363b393422
Tanya-Zheleva <tanya.zhelevaa@gmail.com>
2023-09-27 00:50:17 +0300

    Display mocked home page

---
+++ flight-booking-system/src/lib/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './styles.css';
+import { Layout } from "antd";
+
+const PageFooter = (props) => {
+    return (
+        <Layout.Footer className="page-footer">
+            Footer placeholder
+        </Layout.Footer>
+    )
+}
+
+export default PageFooter;

3cd0243dd8678304381321b2afa03d0a82b8001a
Marina Dudushka <marina.dudushka@sap.com>
2023-10-01 15:07:41 +0300

    WIP

--- flight-booking-system/src/lib/header/index.js
+++ flight-booking-system/src/lib/header/index.js
@@ -1,14 +1,28 @@
 import React from "react"
 import './styles.css';
-import { Layout } from "antd";
-import { ReactComponent as Logo } from '../logo/logo.svg'
+import { AppBar, Icon, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from "@mui/icons-material/Menu";
+import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
 
 const PageHeader = (props) => {
     return (
-        <Layout.Header className="page-header">
-            <Logo />
-            Header placeholder
-        </Layout.Header>
+        <AppBar position="static" >
+            <Toolbar>
+                <Icon>
+                    <ConnectingAirportsIcon classes="page-logo" />
+                </Icon>
+                <Typography sx={{ flexGrow: 1 }} variant="h6" component="div" classes="page-title"> Fligh Booking Site Name </Typography>
+                <IconButton classes="page-menu"
+                    size="large"
+                    edge="end"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 1 }}>
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+
     )
 }
 
